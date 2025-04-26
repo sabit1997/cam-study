b12dc03fdc68f01427608bb607d9f5961d3d814e
YEJI JUNG <yejiii1997@gmail.com>
2025-04-26 15:50:11 +0900

    fix: 에러 구체화

--- components/camera-view.tsx
+++ components/camera-view.tsx
@@ -25,11 +25,11 @@ const CameraView = () => {
   };
 
   const cleanupVideoStream = (videoElement: HTMLVideoElement) => {
+    videoElement.srcObject = null;
     if (streamRef.current) {
       streamRef.current.getTracks().forEach((track) => track.stop());
       streamRef.current = null;
     }
-    videoElement.srcObject = null;
   };
 
   useEffect(() => {
@@ -39,11 +39,23 @@ const CameraView = () => {
         if (!videoElement) return;
 
         const stream = await getCameraStream();
-
         streamRef.current = stream;
         setupVideoStream(videoElement, stream);
       } catch (err) {
-        console.error("카메라 초기화 실패:", err);
+        if (err instanceof DOMException) {
+          switch (err.name) {
+            case "NotAllowedError":
+              console.error("사용자가 카메라 사용을 거부했습니다.");
+              break;
+            case "NotFoundError":
+              console.error("사용할 수 있는 카메라를 찾을 수 없습니다.");
+              break;
+            default:
+              console.error("카메라 접근 중 DOMException 발생:", err);
+          }
+        } else {
+          console.error("카메라 초기화 중 알 수 없는 오류:", err);
+        }
       }
     };
 
